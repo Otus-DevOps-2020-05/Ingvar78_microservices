b9aa0b1ac307fccd2ea3799550cc4248d3c2fbae
Igor V <igor@eger.pro>
2020-09-16 22:08:48 +0300

    HW18 Lecture 24 Pre

--- gitlab-ci/infra/yc_dm-host.sh
+++ gitlab-ci/infra/yc_dm-host.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-docker-machine create \
---driver generic \
---generic-ip-address=130.193.38.227 \
---generic-ssh-user yc-user \
---generic-ssh-key ~/.ssh/ubuntu \
-docker-host
+docker-machine rm docker-host
